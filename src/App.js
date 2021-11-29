cc524de9ada81b63f2805649536337390863af0f
sanishchirayath1 <sanishchirayath@gmail.com>
2021-11-29 12:28:15 +0530

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ export default function App() {
     return (
         <div className="container">
             <Info />
-            <Info />
             <About />
             <Interests />
             <Footer />
