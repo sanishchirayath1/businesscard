5edddd88884f37e24eaa828c0a75ac487e1c8210
Sanish Chirayath <46041082+sanishchirayath1@users.noreply.github.com>
2021-11-29 12:57:42 +0530

    alts and hrefs added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from "react"
 export default function Footer() {
     return (
         <footer className="footer">
-            <a href="#" ><img src="assets/twitter.png" /></a>
-            <a href="#" ><img src="../assets/facebook.png" /></a>
-            <a href="#" ><img src="../assets/insta.png" /></a>
-            <a href="#" ><img src="../assets/github.png" /></a>
+            <a href="https://twitter.com/sanishch" ><img src="assets/twitter.png" alt="logo"/></a>
+            <a href="https://www.facebook.com/schirayath" ><img src="../assets/facebook.png" alt="logo"/></a>
+            <a href="https://www.instagram.com/sanish_sunny_/" ><img src="../assets/insta.png" alt="logo"/></a>
+            <a href="https://github.com/sanishchirayath1" ><img src="../assets/github.png" alt="logo"/></a>
         </footer>
     )
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -3,15 +3,15 @@ import React from "react"
 export default function Info() {
     return (
         <div className="info">
-            <img src="./assets/businessCard.png" />
+            <img src="./assets/businessCard.png" alt="potrait"/>
             <div>
                 <h2> Sanish Chirayath</h2>
                 <p className="designation">Frontend Developer</p>
                 <small className="site">sanishchirayath.portfolio.site</small>
             </div>
             <div className="buttons">
-                <a href="#" className="button"><img src="./assets/email.png" />  Email</a>
-                <a href="#" className="button linkedin"><img src="./assets/linkedin.png" />  LinkedIn</a>          
+                <a href="mailto:sanishchirayath@gmail.com" className="button"><img src="./assets/email.png" />  Email</a>
+                <a href="https://www.linkedin.com/in/sanishchirayath/" className="button linkedin"><img src="./assets/linkedin.png" />  LinkedIn</a>          
             </div>    
         </div>
     )
